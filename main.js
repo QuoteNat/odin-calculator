0d0946a65a30ce4844d732ae3d03e9b978edc52b
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-05-15 13:22:24 -0500

    Store user input

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
+let lhs = "";
+let rhs = "";
+let operator = "";
+let state = "lhs";
+const NUMS = "1234567890".split("");
+const OPERATORS = "+-X/".split("");
+
+function clear() {
+    lhs = "";
+    rhs = "";
+    operator = "";
+    state = "lhs";
+}
+
+// evaluate the entered expression
+function evaluate() {
+
+}
+
 function buttonClick(e) {
-    console.log(e.currentTarget.dataset.value);
+    let input = e.currentTarget.dataset.value;
+    console.log(input);
+    if (input === "clear") {
+        clear();
+    } else if (input === "=") {
+        evaluate();
+    } else if (NUMS.includes(input)) {
+        if (state === "lhs") {
+            lhs += input;
+            console.log("LHS=" + lhs);
+        } else {
+            rhs += input;
+            console.log("RHS=" + rhs);
+        }
+    }
 }
 
 const buttons = document.querySelectorAll(".calculator-button");
